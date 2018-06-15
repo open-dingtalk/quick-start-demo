25e1ed034a9898c47140eb653f4cd65123e0d1e3
opendingtalk <31611459+opendingtalk@users.noreply.github.com>
2018-06-15 11:17:25 +0800

    Update Constant.java
    del suitekey and suitesecret

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,13 +7,13 @@ public class Constant {
     /**
      * 套件ID，登录开发者后台，点击套件管理，进入套件详情可见
      */
-    public static final Long SUITE_ID = 2350002L;
+    public static final Long SUITE_ID = 0L;
     /**
      * 套件的SuiteKey，登录开发者后台，点击套件管理，进入套件详情可见
      */
-    public static final String SUITE_KEY="suitexnh4qtqzl91jjxzr";
+    public static final String SUITE_KEY="";
     /**
      * 套件的SuiteSecret，登录开发者后台，点击套件管理，进入套件详情可见
      */
-    public static final String SUITE_SECRET="5KC5HKsKVKJJkKEgIZ23D3pUgb5IiIoIQZWPTGSi4yvtH7qcD_abHrVUAcs9Icbk";
+    public static final String SUITE_SECRET="";
 }
